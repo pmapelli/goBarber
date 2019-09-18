d9f14c5612e1fea80fab7a59a2c9b6ce979b1c56
pmapelli <pmapelli@hotmail.com>
2019-09-18 14:56:56 -0300

    feat: Create pagination in the schedule list

--- src/app/controllers/AppointementController.js
+++ src/app/controllers/AppointementController.js
@@ -6,9 +6,13 @@ import File from '../models/File';
 
 class AppointementController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointement.findAll({
       where: { user_id: req.userId, canceled_at: null },
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       order: ['date'],
       include: [
         {
