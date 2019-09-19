cd112076e8c274fcbea37baf48fdb7538ed073cd
pmapelli <pmapelli@hotmail.com>
2019-09-19 16:54:15 -0300

    feat: Update notification read

--- src/routes.js
+++ src/routes.js
@@ -31,6 +31,7 @@ routes.post('/appointements', AppointementController.store);
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
